afaf1c60f231e9edf26ec78352df7f861871c606
Raunak <raunaklal@gmail.com>
2018-03-12 23:18:59 +0530

    2nd Commit

--- app/src/main/java/com/raunak/android/cricketscorerandstats/MainActivity.java
+++ app/src/main/java/com/raunak/android/cricketscorerandstats/MainActivity.java
@@ -2,12 +2,31 @@ package com.raunak.android.cricketscorerandstats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mTopToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTopToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(mTopToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
+
