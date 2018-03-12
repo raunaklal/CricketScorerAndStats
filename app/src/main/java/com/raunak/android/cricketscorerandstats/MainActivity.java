090cb7a1566403a4570e56b12bddda98e6e9b79c
Raunak <raunaklal@gmail.com>
2018-03-12 23:23:50 +0530

    2nd Commit

--- app/src/main/java/com/raunak/android/cricketscorerandstats/MainActivity.java
+++ app/src/main/java/com/raunak/android/cricketscorerandstats/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id=item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if(id==R.id.action_favorite){
+            Toast.makeText(MainActivity.this, "Action Clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 }
